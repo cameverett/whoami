7eca832a21e9a598225172e3b72a63df6de5bf56
cameverett <cameronjeverett@gmail.com>
2016-12-11 23:36:06 -0500

    bringing in repository component and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GitHubRepoService } from './github-repo/github-repo.service';
+import Repo from './Models/Repo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  private repos: Repo[];
+
+  constructor(private repoService: GitHubRepoService) {}
+
+  ngOnInit() {
+    this.getRepositories('cameverett');
+  }
+
+  private getRepositories(username: string): void {
+    this.repoService.getRepos(username)
+      .then(repos => this.repos = repos);
+  }
 }
