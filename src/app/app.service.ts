708df192579966f4f76e2e2b888ac8ad8e9bf0f2
cameverett <cameronjeverett@gmail.com>
2016-12-13 00:56:59 -0500

    added user-profile service methods to app.service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,13 +4,15 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import Repo from './Models/Repo';
+import User from './Models/User';
 
 @Injectable()
 export class AppService {
     private repoByUsernameUrl = 'https://api.github.com/users/';
+    private userProfileUrl = 'https://api.github.com/users/';
 
     constructor(private http: Http) { }
-
+    
     getRepos(user: string): Promise<Repo[]> {
         return this.http.get( this.repoByUsernameUrl + user + '/repos?per_page=9')
             .toPromise()
@@ -19,6 +21,17 @@ export class AppService {
             })
             .catch(this.handleError);
     }
+
+    getUser(username: string) : Promise<User> {
+        return this.http.get(this.userProfileUrl + username)
+            .toPromise()
+            .then(response => {
+                return this.mapPropsToUserModel(response);
+            
+            })
+            .catch(this.handleError);
+    }
+    
 
     private mapPropsToRepoModel(response): Repo {
         const repos = response.json();
@@ -30,6 +43,16 @@ export class AppService {
                 url: res.html_url
             }
         })
+    }
+
+    private mapPropsToUserModel(jsonResponse): User {
+        let result = jsonResponse.json();
+        return {
+            bio: result.bio,
+            profileUrl: result.html_url,
+            thumbnail: result.avatar_url,
+            username: result.login
+        }
     }
 
     private handleError(error: any): any {
