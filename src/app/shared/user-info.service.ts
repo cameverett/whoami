76c02ac5944dc1b9f21e1abcb108416f82c5fab2
cameverett <cameronjeverett@gmail.com>
2017-01-13 18:56:06 -0500

    Added index to Models for convenience.

--- src/app/shared/user-info.service.ts
+++ src/app/shared/user-info.service.ts
@@ -1,7 +1,35 @@
 import { Injectable } from '@angular/core';
 import { AppGitHubService } from './app-github.service';
 
+import { Activity, Repo, User } from '../Models/index';
+
 @Injectable()
 export class UserInfoService {
-    constructor() { }
+    private activities: Array<Activity>;
+    private repos: Array<Repo>;
+    private user: User;
+
+    constructor(private _http: AppGitHubService) { }
+
+    public loadNewUserInfo(username: string): void {
+        this._http.loadUserInfo(username)
+            .subscribe(
+                res => {
+                    this.activities = res.activities,
+                    this.repos = res.repos,
+                    this.user = res.user
+                },
+                err => console.log(err)
+            )
+    }
+
+    public get userActivities(): Array<Activity> {
+        return this.activities;
+    }
+    public get userRepos(): Array<Repo> {
+        return this.repos;
+    }
+    public get userProfile(): User {
+        return this.user;
+    }
 }
