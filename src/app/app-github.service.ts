956e9a058867c0d7987ea0ca471b4c2d71235034
cameverett <cameronjeverett@gmail.com>
2017-01-13 03:40:54 -0500

    Learned something new with importing rxjs features

--- src/app/app-github.service.ts
+++ src/app/app-github.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, URLSearchParams } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
-import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/forkJoin';
+
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/retry';
 
 import { Activity } from './Models/Activity';
 import { Repo } from './Models/Repo';
